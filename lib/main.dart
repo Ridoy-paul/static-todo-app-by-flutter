1b4a88ae748af3429208617ef26a2a0ea83321c2
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-10-09 03:08:02 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -33,19 +33,19 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Color(0xffd9dae0),
       appBar: AppBar(
         title: const Text("Awesome ToDo"),
       ),
       body: SafeArea(
         child: Container(
-          margin: const EdgeInsets.all(10),
+          margin: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
           child: ListView.builder(
             itemCount: 100,
             itemBuilder: (BuildContext context, index) {
               index++;
               return GestureDetector(
-                onTap: (){
+                onTap: () {
                   print("$index");
                 },
                 child: Card(
@@ -59,15 +59,52 @@ class _HomePageState extends State<HomePage> {
                   ),
                   margin: const EdgeInsets.only(bottom: 8),
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        child: Text("$index"),
-                      ),
-                      contentPadding: const EdgeInsets.all(2),
-                      title: Text("Hello Todo"),
-                    )
-                  ),
+                      padding: const EdgeInsets.all(5.0),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Text("$index"),
+                        ),
+                        contentPadding: const EdgeInsets.all(2),
+                        title: Text("Hello Todo  sdfj sfk jsfk jsf jks"),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: Text("17 Jan 2023 8:10:33 AM"),
+                        ),
+                        trailing: Wrap(
+                          alignment: WrapAlignment.start,
+                          direction: Axis.vertical,
+                          runSpacing: 5,
+                          children: [
+                            GestureDetector(
+                              child: Tooltip(
+                                message: "Edit",
+                                preferBelow: false,
+                                child: const Icon(Icons.edit_calendar,
+                                    color: Colors.red,
+                                ),
+                              ),
+                              // Icon for the button
+                              onTap: () {
+                                print("Edit todo");
+                              },
+                            ),
+                           
+                            GestureDetector(
+                              child: Tooltip(
+                                message: "Delete",
+                                preferBelow: true,
+                                child: const Icon(Icons.delete_forever,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              // Icon for the button
+                              onTap: () {
+                                print("Delete todo");
+                              },
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
               );
             },
