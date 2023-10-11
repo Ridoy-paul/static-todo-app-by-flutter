fd34991681801d3e7b2619f5c2ab1db8283464c3
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-10-12 00:37:05 +0600

    updated

---
+++ lib/add_new_task_modal.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class AddToDoModal extends StatelessWidget {
+  const AddToDoModal({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 5),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      //height: MediaQuery.sizeOf(context).height * .9,
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Add New Task",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.close),
+              )
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            //height: MediaQuery.sizeOf(context).height * 0.4,
+            child: TextFormField(
+              //keyboardType: TextInputType.multiline,
+              maxLines: 6,
+              decoration: const InputDecoration(
+                hintText: "Enter Your Task",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          SizedBox(
+            height: 50,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  )),
+              child: const Text("Add Task"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'add_new_task_modal.dart';
+import 'update_task_modal.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -128,7 +130,7 @@ class TaskItem extends StatelessWidget {
                                         isScrollControlled: true,
                                         context: context,
                                         builder: (context) {
-                                          return UpdateToDoModal();
+                                          return const UpdateToDoModal();
                                         },
                                       );
                                     },
@@ -164,144 +166,6 @@ class TaskItem extends StatelessWidget {
                 },
               ),
             )),
-      ),
-    );
-  }
-}
-
-class AddToDoModal extends StatelessWidget {
-  const AddToDoModal({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 5),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
-      ),
-      //height: MediaQuery.sizeOf(context).height * .9,
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Add New Task",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.close),
-              )
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 10),
-            //height: MediaQuery.sizeOf(context).height * 0.4,
-            child: TextFormField(
-              //keyboardType: TextInputType.multiline,
-              maxLines: 6,
-              decoration: const InputDecoration(
-                hintText: "Enter Your Task",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          SizedBox(
-            height: 50,
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100),
-              )),
-              child: const Text("Add Task"),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class UpdateToDoModal extends StatelessWidget {
-  const UpdateToDoModal({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 5),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
-      ),
-      //height: MediaQuery.sizeOf(context).height * .9,
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Update Task",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.close),
-              )
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 10),
-            //height: MediaQuery.sizeOf(context).height * 0.4,
-            child: TextFormField(
-              //keyboardType: TextInputType.multiline,
-              maxLines: 6,
-              decoration: const InputDecoration(
-                hintText: "Enter Your Task",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          SizedBox(
-            height: 50,
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100),
-              )),
-              child: const Text("Update"),
-            ),
-          ),
-        ],
       ),
     );
   }
